0989ba34479c0093d0072bcf0f4a0a9834874589
pharmsegun <segunolawalekogbe@yahoo.com>
2023-06-19 10:34:51 -0700

    Print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		put char (lowercase);
+		lowercase += 1;
+	}
+	put char ('\n');
+	return (0);
+}
+
