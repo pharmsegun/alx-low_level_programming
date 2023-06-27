9b8adc47a954bea57d9df4b3531cc0f530980f6f
pharmsegun <segunolawalekogbe@yahoo.com>
2023-06-27 03:19:00 -0700

    returns the length of a string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * -strlen - returns the length of a string
+ * _strlen - returns the length of a string
  *  @s: string
  *  Return: length
  */
@@ -8,10 +8,10 @@ int _strlen(char *s)
 {
 	int longi = 0;
 
-	while(*s != '\0')
+	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
-	return(longi);
+	return (longi);
 }
